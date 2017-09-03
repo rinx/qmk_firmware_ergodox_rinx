cf21da8ce9aa631304c920d0c2761222094a21b3
Rintaro Okamura <rintaro.okamura@gmail.com>
2017-09-04 06:51:15 +0900

    Revise keymap

--- keymap.c
+++ keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | SPOT |           |MISSIO|   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------| LIGHT|           |NCTRL |------+------+------+------+------+--------|
- * |Esc/Ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | "/Ctrl |
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | "/Ctrl |
  * |--------+------+------+------+------+------| PREV |           | NEXT |------+------+------+------+------+--------|
  * |Shift/[ |Rais/Z|   X  |   C  |   V  |   B  | WKS  |           | WKS  |   N  |   M  |   ,  |   .  |Lowr//|Shift/] |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         M(MAC_COPY_PASTE), KC_1, KC_2,   KC_3,   KC_4,   KC_5,   DYN_MACRO_PLAY1,
         KC_TAB,      KC_Q,     KC_W,     KC_E,   KC_R,   KC_T,   SPTLGHT,
-        CTL_T(KC_ESC),     KC_A,     KC_S,     KC_D,   KC_F,   KC_G,
+        KC_LCTL,     KC_A,     KC_S,     KC_D,   KC_F,   KC_G,
         SFT_T(KC_LBRC), LT(_RAIS, KC_Z), KC_X,   KC_C,   KC_V,   KC_B,   PREVWKS,
         TT(_ADJUST),    LT(_FUNCT, KC_LEFT),   KC_LALT,  KC_LGUI,KC_DOWN,
                                                        KC_TAB,  PREVTAB,
@@ -176,9 +176,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  F1    |  F2  |  F3  |  F4  |  F5  |  F6  |      |           |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |M C/P |M COPY|MPASTE|      |------|           |------|      |      |DMPLY1|DMREC1|DMSTOP|        |
+ * |        |      |M C/P |M COPY|MPASTE|      |------|           |------|      |DMPLY1|DMREC1|DMSTOP|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |W C/P |W COPY|WPASTE|      |      |           |      |      |      |DMPLY2|DMREC2|      |        |
+ * |        |      |W C/P |W COPY|WPASTE|      |      |           |      |      |DMPLY2|DMREC2|      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -204,8 +204,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // right hand
        DYN_REC_START2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-                KC_TRNS, KC_TRNS, DYN_MACRO_PLAY1, DYN_REC_START1, DYN_REC_STOP, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, DYN_MACRO_PLAY2, DYN_REC_START2, KC_TRNS, KC_TRNS,
+                KC_TRNS, DYN_MACRO_PLAY1, DYN_REC_START1, DYN_REC_STOP, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, DYN_MACRO_PLAY2, DYN_REC_START2, KC_TRNS, KC_TRNS, KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
